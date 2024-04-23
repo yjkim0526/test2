0fa42b41664907c0c08b1d64e06a54355f980bd4
kimyujin <daeguforever@gmail.com>
2024-04-24 00:18:26 +0900

    login 처리

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -2,3 +2,21 @@
 
 console.log("Hello");
 console.log("bye");
+
+const id = document.querySelector("#id");
+const passwd = document.querySelector("#psword");
+const loginBtn = document.querySelector("button");
+
+console.log(id);
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+  console.log("login()");
+  console.log(id.value);
+  const req = {
+    id: id.value,
+    psword: psword.value,
+  };
+  console.log(req);
+}
